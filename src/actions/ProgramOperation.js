34a6a1f491b352a723a32b07073a226724b1940b
Jad-kad <kaddour.jad@gmail.com>
2017-10-03 21:31:31 +0200

    fixing-syntax-error-bug

--- src/actions/ProgramOperation.js
+++ src/actions/ProgramOperation.js
@@ -3,36 +3,42 @@ import * as fn from './actions'
 import * as C from '../calculator-codes/ProgramTextCode'
 
 export default function programOperation(newProg) {
-
     let textValue = store.state.textAreaValue,
-        
+
         newArray1 = textValue.replace(/^\s*\n/gm, ""),
         newArray = newArray1.split(/\n/)
 
     console.log('text value', newArray)
     for (let i = 0; i < newArray.length; i++) {
-        let [x, y, z, t] = store.state.stack
-        if ((Number(newArray[i]) || newArray[i] === '0')) {
 
-            console.log('newArray', 'newArray')
+        let [x, y, z, t] = store.state.stack
+
+        if ((Number(newArray[i]) || newArray[i] === '0')) {
 
             let localStack = [newArray[i], x, y, z]
 
             store.setState({ stack: localStack, textAreaValue: newProg })
+
         } else if (C.textObject[newArray[i]]) {
+
             let codeText = C.textObject[newArray[i]]
-            console.log('text code', codeText)
+            // console.log('text code', codeText)
+
             fn.operations(codeText)
+
         } else {
+            
             newArray[i] = newArray[i] + ' => syntax error'
-            // console.log(newArray[i])
-            // console.log('newarray',newArray)
-            newProg = newArray.join('\n')
 
-            // console.log(newProg)
-            store.setState({ textAreaValue: newProg })
-            return
+            if (newArray.indexOf('') !== -1) {
+
+                newArray.pop()
+
+                newProg = newArray.join('\n')
+
+                store.setState({ textAreaValue: newProg })
+            }
         }
-    }
 
+    }
 }
