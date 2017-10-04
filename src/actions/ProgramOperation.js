7a0757d4c639b5f98667cc608e34a1610ea06974
Jad-kad <kaddour.jad@gmail.com>
2017-10-04 12:57:29 +0200

    updating-KeyCodes

--- src/actions/ProgramOperation.js
+++ src/actions/ProgramOperation.js
@@ -1,14 +1,16 @@
 import store from '../store'
 import * as fn from './actions'
-import * as C from '../calculator-codes/ProgramTextCode'
+import {textObject1} from '../calculator-codes/ProgramTextCode'
 
 export default function programOperation(newProg) {
+
     let textValue = store.state.textAreaValue,
 
         newArray1 = textValue.replace(/^\s*\n/gm, ""),
         newArray = newArray1.split(/\n/)
 
-    console.log('text value', newArray)
+    // console.log('text value', newArray)
+
     for (let i = 0; i < newArray.length; i++) {
 
         let [x, y, z, t] = store.state.stack
@@ -18,16 +20,16 @@ export default function programOperation(newProg) {
             let localStack = [newArray[i], x, y, z]
 
             store.setState({ stack: localStack, textAreaValue: newProg })
-
-        } else if (C.textObject[newArray[i]]) {
-
-            let codeText = C.textObject[newArray[i]]
-            // console.log('text code', codeText)
+            
+        } else if (textObject1.indexOf(newArray[i])) {
+            console.log('2222',textObject1.indexOf(newArray[i]))
+            let codeText = newArray[i]
+             console.log('text code', codeText)
 
             fn.operations(codeText)
 
         } else {
-            
+
             newArray[i] = newArray[i] + ' => syntax error'
 
             if (newArray.indexOf('') !== -1) {
--- src/components/KeyBoard.js
+++ src/components/KeyBoard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import CalculatorKey from './CalculatorKey'
-import * as C from '../calculator-codes/KeyCodes'
+import  {KC} from '../calculator-codes/KeyCodes'
 
 export default class KeyBoard extends React.Component {
 	render() {
@@ -8,59 +8,59 @@ export default class KeyBoard extends React.Component {
 		<div className="mathTable">	
 			<div className='KeyBoardPad'>
 			  <div>
-			  	<CalculatorKey label='xʸ' keyCode={C.POW}/>
-			  	<CalculatorKey label='LOG' keyCode={C.LOG} />
-			  	<CalculatorKey label='LN' keyCode={C.LN} />
-			  	<CalculatorKey label='eˣ' keyCode={C.EXP} />
-			  	<CalculatorKey label='CLR' keyCode={C.CLR} />
+			  	<CalculatorKey label='xʸ' keyCode={KC.POW}/>
+			  	<CalculatorKey label='LOG' keyCode={KC.LOG} />
+			  	<CalculatorKey label='LN' keyCode={KC.LN} />
+			  	<CalculatorKey label='eˣ' keyCode={KC.EXP} />
+			  	<CalculatorKey label='CLR' keyCode={KC.CLR} />
 			  </div>
 			  <div>
-			  	<CalculatorKey label='√x' keyCode={C.SQRT} />
-			  	<CalculatorKey label='ARC' keyCode={C.ARC} />
-			  	<CalculatorKey label='SIN' keyCode={C.SIN} />
-			  	<CalculatorKey label='COS' keyCode={C.COS} />
-			  	<CalculatorKey label='TAN' keyCode={C.TAN} />
+			  	<CalculatorKey label='√x' keyCode={KC.SQRT} />
+			  	<CalculatorKey label='ARC' keyCode={KC.ARC} />
+			  	<CalculatorKey label='SIN' keyCode={KC.SIN} />
+			  	<CalculatorKey label='COS' keyCode={KC.COS} />
+			  	<CalculatorKey label='TAN' keyCode={KC.TAN} />
 			  </div>
 			  <div>
-			  	<CalculatorKey label='¹/x' keyCode={C.RECIPROCAL} />
-			  	<CalculatorKey label='x↔︎y' keyCode={C.SWAP} />
-			  	<CalculatorKey label='R↓' keyCode={C.ROLL_DOWN} />
-			  	<CalculatorKey label='STO' keyCode={C.STO} />
-			  	<CalculatorKey label='RCL' keyCode={C.RCL} />
+			  	<CalculatorKey label='¹/x' keyCode={KC.RECIPROCAL} />
+			  	<CalculatorKey label='x↔︎y' keyCode={KC.SWAP} />
+			  	<CalculatorKey label='R↓' keyCode={KC.ROLL_DOWN} />
+			  	<CalculatorKey label='STO' keyCode={KC.STO} />
+			  	<CalculatorKey label='RCL' keyCode={KC.RCL} />
 			  </div>
 			  <div>
-			  	<CalculatorKey label='Enter ↑' keyCode={C.ENTER} />
-			  	<CalculatorKey label='CHS' keyCode={C.CHS} />
-			  	<CalculatorKey label='EEX' keyCode={C.EEX} />
-			  	<CalculatorKey label='CLX' keyCode={C.CLX} />
+			  	<CalculatorKey label='Enter ↑' keyCode={KC.ENTER} />
+			  	<CalculatorKey label='CHS' keyCode={KC.CHS} />
+			  	<CalculatorKey label='EEX' keyCode={KC.EEX} />
+			  	<CalculatorKey label='CLX' keyCode={KC.CLX} />
 			  </div>
 			  <div>
-			  	<CalculatorKey label='-' keyCode={C.SUB} />
-				<CalculatorKey label='g' keyCode={C.SUB} />  
-			  	<CalculatorKey label='7' keyCode={C.D7} />
-			  	<CalculatorKey label='8' keyCode={C.D8} />
-			  	<CalculatorKey label='9' keyCode={C.D9} />
+			  	<CalculatorKey label='-' keyCode={KC.SUB} />
+				<CalculatorKey label='g' keyCode={KC.SUB} />  
+			  	<CalculatorKey label='7' keyCode={KC.D7} />
+			  	<CalculatorKey label='8' keyCode={KC.D8} />
+			  	<CalculatorKey label='9' keyCode={KC.D9} />
 			  </div>
 			  <div>
-			  	<CalculatorKey label='+' keyCode={C.ADD} />
-				<CalculatorKey label='c' keyCode={C.ADD} />   
-			  	<CalculatorKey label='4' keyCode={C.D4} />
-			  	<CalculatorKey label='5' keyCode={C.D5} />
-			  	<CalculatorKey label='6' keyCode={C.D6} />
+			  	<CalculatorKey label='+' keyCode={KC.ADD} />
+				<CalculatorKey label='c' keyCode={KC.ADD} />   
+			  	<CalculatorKey label='4' keyCode={KC.D4} />
+			  	<CalculatorKey label='5' keyCode={KC.D5} />
+			  	<CalculatorKey label='6' keyCode={KC.D6} />
 			  </div>
 			  <div>
-			  	<CalculatorKey label='×' keyCode={C.MUL} />
-				<CalculatorKey label='h' keyCode={C.ADD} />  
-			  	<CalculatorKey label='1' keyCode={C.D1} />
-			  	<CalculatorKey label='2' keyCode={C.D2} />
-			  	<CalculatorKey label='3' keyCode={C.D3} />
+			  	<CalculatorKey label='×' keyCode={KC.MUL} />
+				<CalculatorKey label='h' keyCode={KC.ADD} />  
+			  	<CalculatorKey label='1' keyCode={KC.D1} />
+			  	<CalculatorKey label='2' keyCode={KC.D2} />
+			  	<CalculatorKey label='3' keyCode={KC.D3} />
 			  </div>
 			  <div>
-			  	<CalculatorKey label='÷' keyCode={C.DIV} />
-				<CalculatorKey label='?' keyCode={C.ADD} />  
-			  	<CalculatorKey label='0' keyCode={C.D0} />
-			  	<CalculatorKey label='.' keyCode={C.DOT} />
-			  	<CalculatorKey label='π' keyCode={C.PI} />
+			  	<CalculatorKey label='÷' keyCode={KC.DIV} />
+				<CalculatorKey label='?' keyCode={KC.ADD} />  
+			  	<CalculatorKey label='0' keyCode={KC.D0} />
+			  	<CalculatorKey label='.' keyCode={KC.DOT} />
+			  	<CalculatorKey label='π' keyCode={KC.PI} />
 			  </div>
 			</div>
 		   </div>
